4c9b7de10fbc0eb36c24ae5d415896ce5c131f01
yschindel <55532939+yschindel@users.noreply.github.com>
2023-06-21 18:13:27 +0200

    add color tests back

---
+++ Core/Core.TestIntegration/ColorTests/ColorTests.cs
@@ -0,0 +1,50 @@
+﻿using Calc.Core.Color;
+
+namespace Calc.Core.IntegrationTests
+{
+    [TestClass]
+    public class ColorTests
+    {
+        [TestMethod]
+        public void TestColorGradient()
+        {
+            // Arrange
+
+            // Act
+            var colorGradient = new ColorGradient(10);
+            var hslColors = colorGradient.HslColors;
+
+            // Assert
+            Assert.AreEqual(10, hslColors.Count);
+        }
+
+        [TestMethod]
+        public void TestHslToRgb()
+        {
+            // Arrange
+            // make a red color
+            var hslColor = new HslColor(0, 100, 50);
+
+            // Act
+            var rgbColor = ColorConverter.HslToRgb(hslColor);
+
+            // Assert
+            Assert.AreEqual(255, rgbColor.Red);
+            Assert.AreEqual(0, rgbColor.Green);
+            Assert.AreEqual(0, rgbColor.Blue);
+        }
+
+        [TestMethod]
+        public void TestRgbToHex()
+        {
+            // Arrange
+            var rgbColor = new RgbColor(255, 0, 0);
+
+            // Act
+            var hexColor = ColorConverter.RgbToHex(rgbColor);
+
+            // Assert
+            Assert.AreEqual("#FF0000", hexColor.Hex);
+        }
+    }
+}
