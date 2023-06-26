4969edabe8e28d4d1565c88376cd8ef8a0463006
yschindel <55532939+yschindel@users.noreply.github.com>
2023-06-26 14:10:18 +0200

    Add coloring by buildup

---
+++ Core/Core/Color/BranchPainter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Speckle.Newtonsoft.Json;
+using Calc.Core.Objects;
+using System.Linq;
+
+namespace Calc.Core.Color
+{
+    public class BranchPainter
+    {
+        public static void ColorBranchesByBranch(List<Branch> branches)
+        {
+            if (branches.Count == 0)
+            {
+                Console.WriteLine("No branches to color.");
+                return;
+            }
+
+            var colorGradient = new ColorGradient(branches.Count);
+            for (int index = 0; index < branches.Count; index++)
+            {
+                branches[index].HslColor = colorGradient.HslColors[index];
+                ColorBranchesByBranch(branches[index].SubBranches);
+            }
+        }
+
+        public static void ColorBranchesByBuildup(List<Branch> branches)
+        {
+            var flattenedBranches = new List<Branch>();
+            foreach (var branch in branches)
+            {
+                flattenedBranches.AddRange(branch.Flatten());
+            }
+
+            var uniqueBuildups = flattenedBranches.Select(b => b.Buildup).Distinct().ToList();
+            var colorGradient = new ColorGradient(uniqueBuildups.Count);
+            var defaultColor = new HslColor(0, 0, 100);
+
+            for (int i = 0; i < flattenedBranches.Count; i++)
+            {
+                var branch = flattenedBranches[i];
+                var buildupIndex = uniqueBuildups.IndexOf(branch.Buildup);
+                if (buildupIndex >= 0)
+                {
+                    branch.HslColor = colorGradient.HslColors[buildupIndex];
+                }
+                else
+                {
+                    branch.HslColor = defaultColor;
+                }
+            }
+        }
+    }
+}
