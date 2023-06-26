00a235c886ac39a915385f246cbd36642720d785
yschindel <55532939+yschindel@users.noreply.github.com>
2023-06-26 14:12:35 +0200

    Update Forest.cs

--- Core/Core/Objects/Forest.cs
+++ Core/Core/Objects/Forest.cs
@@ -18,7 +18,7 @@ namespace Calc.Core.Objects
         [JsonProperty("project_id")]
         public Project Project { get; set; }
 
-        public void ColorBranches(string method)
+        public void ColorBranchesBy(string method)
         {
             List<Branch> branches = this.Trees.ConvertAll(tree => (Branch)tree);
 
