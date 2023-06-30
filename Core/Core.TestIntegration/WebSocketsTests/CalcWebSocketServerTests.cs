7295ea78eba6ea0218afe8d222906ae1bf00e242
yschindel <55532939+yschindel@users.noreply.github.com>
2023-06-30 11:52:59 +0200

    Update CalcWebSocketServerTests.cs

--- Core/Core.TestIntegration/WebSocketsTests/CalcWebSocketServerTests.cs
+++ Core/Core.TestIntegration/WebSocketsTests/CalcWebSocketServerTests.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using Calc.Core.Objects;
 using Calc.Core.WebSockets;
-using Fleck;
 using Speckle.Newtonsoft.Json;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
