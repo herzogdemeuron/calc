419fd889a530e3978b03646a2ce7cf57219c310e
yschindel <55532939+yschindel@users.noreply.github.com>
2023-06-30 10:52:45 +0200

    remove IFilter

--- Core/Core/Objects/ObjectInterfaces.cs
+++ Core/Core/Objects/ObjectInterfaces.cs
@@ -7,15 +7,6 @@ using System.Threading.Tasks;
 
 namespace Calc.Core.Objects
 {
-    public interface IFilter
-    {
-        List<CalcElement> Elements { get; }
-        List<string> ElementIds { get; }
-        string Parameter { get; set; }
-        string Method { get; set; }
-        string Value { get; set; }
-    }
-
     public interface IHasProject
     {
         Project Project { get; set; }
