fa760d60d295ec0d08828c04bf6e92dcf55b786c
yschindel <55532939+yschindel@users.noreply.github.com>
2023-07-03 16:03:08 +0200

    add disconnect behavoir

--- Core/calc-vis-interactive/src/App.vue
+++ Core/calc-vis-interactive/src/App.vue
@@ -25,46 +25,57 @@ export default {
   },
   methods: {
     connectWebSocket() {
-      if (this.socket !== null && this.socket.readyState === WebSocket.OPEN) {
-        // WebSocket is already open, no need to reconnect
-        return;
-      }
+  if (this.socket !== null && this.socket.readyState === WebSocket.OPEN) {
+    // WebSocket is already open, no need to reconnect
+    return;
+  }
 
-      this.socket = new WebSocket('ws://127.0.0.1:8184');
+  this.socket = new WebSocket('ws://127.0.0.1:8184');
 
-      this.socket.onopen = () => {
-        let time = new Date().toLocaleTimeString();
-        this.socket.send(`${time} cal-viz-interactive connected`);
-      };
+  this.socket.onopen = () => {
+    let time = new Date().toLocaleTimeString();
+    this.socket.send(`${time} cal-viz-interactive connected`);
+  };
 
-      this.socket.onmessage = (event) => {
-        console.log('Message received from server');
-        this.handleWebSocketMessage(event.data);
-      };
+  this.socket.onmessage = (event) => {
+    console.log('Message received from server');
+    this.handleWebSocketMessage(event.data);
+  };
 
-      this.socket.onclose = (event) => {
-        if (event.wasClean) {
-          console.log(`Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-        } else {
-          console.log('Connection died');
-        }
-        
-        // Attempt to reconnect after a delay (e.g., 5 seconds)
-        setTimeout(() => {
-          this.connectWebSocket();
-        }, 5000);
-      };
-
-      this.socket.onerror = (error) => {
-        console.log(`WebSocket Error: ${error.message}`);
-        // Handle the error as needed
-      };
-    },
-    handleWebSocketMessage(data) {
-      // Assuming the received data is in JSON format
-      const receivedData = JSON.parse(data);
-      this.dataset = reactive(receivedData);
+  this.socket.onclose = (event) => {
+    if (event.wasClean) {
+      console.log(`Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+      console.log('Connection died');
     }
+
+    // Attempt to reconnect after a delay (e.g., 5 seconds)
+    setTimeout(() => {
+      this.connectWebSocket();
+    }, 5000);
+  };
+
+  this.socket.onerror = (error) => {
+    console.log(`WebSocket Error: ${error.message}`);
+    // Handle the error as needed
+  };
+
+  // Add event listener for beforeunload event
+  window.addEventListener('beforeunload', () => {
+    if (this.socket.readyState === WebSocket.OPEN) {
+      // WebSocket is still open, send disconnection message to the server
+      this.socket.send('calc-viz-interactive disconnected');
+      this.socket.close();
+    }
+  });
+},
+
+handleWebSocketMessage(data) {
+  // Assuming the received data is in JSON format
+  const receivedData = JSON.parse(data);
+  this.dataset = reactive(receivedData);
+}
+
   }
 };
 </script>
