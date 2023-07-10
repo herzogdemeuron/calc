83ebcefd580fa422151d5d018d9be2e6b31e916f
yschindel <55532939+yschindel@users.noreply.github.com>
2023-07-10 16:33:10 +0200

    fix dll loading

--- ConnectorRevit/ConnectorRevit2023/Revit/StartCommand.cs
+++ ConnectorRevit/ConnectorRevit2023/Revit/StartCommand.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Diagnostics;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.Attributes;
 using Calc.ConnectorRevit.Views;
 using Calc.Core;
+using System.Reflection;
 
 namespace Calc.ConnectorRevit.Revit
 {
@@ -15,6 +17,7 @@ namespace Calc.ConnectorRevit.Revit
         {
             try
             {
+                AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
                 App.CurrentDoc = commandData.Application.ActiveUIDocument.Document;
                 App.ViewModel = new ViewModel();
                 MainView mainView = new MainView();
@@ -26,6 +29,19 @@ namespace Calc.ConnectorRevit.Revit
                 Debug.WriteLine(ex);
                 return Result.Failed;
             }
+        }
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string assemblyFolder = @"C:\ProgramData\Autodesk\Revit\Addins\2023\CalcRevit2023\Release"; // Specify the directory where your DLLs are located
+
+            string assemblyName = new AssemblyName(args.Name).Name;
+            string assemblyPath = Path.Combine(assemblyFolder, assemblyName + ".dll");
+
+            if (File.Exists(assemblyPath))
+                return Assembly.LoadFrom(assemblyPath);
+
+            return null;
         }
     }
 }
