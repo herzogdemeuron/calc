fc0df63e1368a491fc567deb7e573c6863e507d1
k.luo <k.luo@herzogdemeuron.com>
2023-07-19 10:33:22 +0200

    finished refactoring

--- ConnectorRevit/ConnectorRevit2023/Helpers/ForestHelper.cs
+++ ConnectorRevit/ConnectorRevit2023/Helpers/ForestHelper.cs
@@ -13,10 +13,32 @@ namespace Calc.ConnectorRevit.Helpers
     {
         public static void PlantTrees(Forest forest)
         {
+            List<string> parameters = GetParameterList(forest);
+            List<string> BacketElementIds = new List<string>();
             foreach (var t in forest.Trees)
             {
-                t.Plant(ElementFilter.GetCalcElements(t));
+                //List<CalcElement> calcElements = ElementFilter.GetCalcElements(t, parameters, createdRevitElementIds);
+                List<CalcElement> calcElements = ElementFilter.GetCalcElements(t);
+                t.Plant(calcElements);
             }
+        }
+
+        private static List<string> GetParameterList(Forest forest)
+        {
+            //get the root parameters from each tree
+            var parameters = new List<string>();
+            foreach (var t in forest.Trees)
+            {
+               List<Root> roots = t.Roots;
+                foreach (var r in roots)
+                {
+                    if (!parameters.Contains(r.Parameter))
+                    {
+                        parameters.Add(r.Parameter);
+                    }
+                }
+            }
+            return parameters;
         }
     }
 }
