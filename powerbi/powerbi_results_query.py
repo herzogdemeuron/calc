50a5c34bb47e7284339e658cfa214ff4936fbdab
amarrujo <104207048+amarrujo@users.noreply.github.com>
2023-08-11 11:53:33 +0200

    Fixed url

--- powerbi/powerbi_results_query.py
+++ powerbi/powerbi_results_query.py
@@ -5,9 +5,9 @@ import pandas as pd
 token = "<your-directus-token>"
 
 # set url here:
-url = "https://<your-project-name>.directus.app/items"
+url = "https://<your-project-name>.directus.app"
 
 headers = {'Accept': 'application/json', 'Authorization': 'Bearer {}'.format(token), 'Content-Type': 'application/json'}
-res = requests.get("{}/calc_calculation_results".format(url), headers=headers, allow_redirects=False, params={"limit": 300, "sort": "-date_created"})
+res = requests.get("{}/items/calc_calculation_results".format(url), headers=headers, allow_redirects=False, params={"limit": 300, "sort": "-date_created"})
 data = res.json()["data"]
 directus = pd.json_normalize(data, max_level=0)
