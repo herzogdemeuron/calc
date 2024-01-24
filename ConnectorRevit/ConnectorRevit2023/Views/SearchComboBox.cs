7fc53182cff28612ee6be03b6517e97103e68bd6
k.luo <k.luo@herzogdemeuron.com>
2024-01-24 17:43:33 +0100

    search mapping change

--- ConnectorRevit/ConnectorRevit2023/Views/SearchComboBox.cs
+++ ConnectorRevit/ConnectorRevit2023/Views/SearchComboBox.cs
@@ -3,6 +3,7 @@ using System;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Calc.ConnectorRevit.Views
 {
@@ -12,13 +13,45 @@ namespace Calc.ConnectorRevit.Views
         object _lastSelectedItem;
         bool _firstFocus = true;
         bool _ignoreTextChange = false;
+
+        public SearchComboBox()
+        {
+            this.PreviewKeyDown += SearchComboBox_PreviewKeyDown;
+        }
+
+        private void SearchComboBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Check if a character key was pressed and the dropdown is open
+            var c = e.Key.ToString().ToLower();
+            string input = c.Length == 1 ? c : string.Empty;
+            if (this.IsDropDownOpen && _firstFocus)
+            {
+                if (_editableTextBox != null)
+                {
+                    this.IsEditable = true;
+                    _editableTextBox.Focusable = true;
+                    _editableTextBox.Focus();
+                    _editableTextBox.Clear();
+
+                    e.Handled = true;
+
+                    // Simulate the text input
+                    _editableTextBox.RaiseEvent(new TextCompositionEventArgs(InputManager.Current.PrimaryKeyboardDevice, new TextComposition(InputManager.Current, _editableTextBox, input))
+                    {
+                        RoutedEvent = TextCompositionManager.TextInputEvent
+                    });
+
+                    _firstFocus = false;
+                }
+            }
+        }
+
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
             _editableTextBox = GetTemplateChild("PART_EditableTextBox") as TextBox;
             this.IsTextSearchEnabled = false;
             this.StaysOpenOnEdit = true;
-
             if (_editableTextBox != null)
             {
                 _editableTextBox.TextChanged += EditableTextBox_TextChanged;
@@ -26,10 +59,14 @@ namespace Calc.ConnectorRevit.Views
             }
         }
 
+ 
+
         protected override void OnDropDownOpened(EventArgs e)
         {
+            _firstFocus = true;
             _lastSelectedItem = this.SelectedItem;
-            this.IsEditable = true;
+            _editableTextBox.Focusable = false;
+
             if (_editableTextBox == null)
             {
                 this.ApplyTemplate();
@@ -39,18 +76,18 @@ namespace Calc.ConnectorRevit.Views
         protected override void OnDropDownClosed(EventArgs e)
         {
             base.OnDropDownClosed(e);
-
-            if (this.Items.Count > 0 && _editableTextBox.Text.Trim() != string.Empty && _firstFocus == false && Items.Count > 0)
+            /*if (this.Items.Count > 0 && _editableTextBox.Text.Trim() != string.Empty && _firstFocus == false && Items.Count > 0)
             {
                 this.SelectedIndex = 0;
-            }
-            else
+            }*/
+            if (this.SelectedItem == null && _lastSelectedItem != null)
             {
                 this.SelectedItem = _lastSelectedItem;
             }
+
             this.IsEditable = false;
             _firstFocus = true;
-
+            _editableTextBox.Text= string.Empty;
             ResetFilter();
         }
 
@@ -94,14 +131,8 @@ namespace Calc.ConnectorRevit.Views
                         var groupContains = buildup.Group != null && buildup.Group.Name.ToLower().Contains(searchText);
                         return nameContains || groupContains;
                     });
-
-                    /*if (_lastSelectedItem != null && ICV.Contains(_lastSelectedItem))
-                    {
-                        SelectedItem = _lastSelectedItem;
-                    }*/
                 }
             }
-            
         }
 
         private void ResetFilter()
