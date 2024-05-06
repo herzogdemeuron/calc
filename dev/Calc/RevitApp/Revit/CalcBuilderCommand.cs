52ae421cc1d123537b4ba59043ee84fa2a7fb461
k.luo <k.luo@herzogdemeuron.com>
2024-05-06 16:54:03 +0200

    assembly resolving for pyrevit invoke button

--- dev/Calc/RevitApp/Revit/CalcBuilderCommand.cs
+++ dev/Calc/RevitApp/Revit/CalcBuilderCommand.cs
@@ -1,32 +1,30 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Calc.Core;
-using Calc.Core.DirectusAPI;
-using Calc.Core.Objects.Buildups;
-using Calc.MVVM.Services;
 using Calc.MVVM.ViewModels;
 using Calc.MVVM.Views;
 using Calc.RevitConnector.Revit;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
-using System.Threading.Tasks;
-using System.Windows.Controls;
 
 namespace Calc.RevitApp.Revit
 {
     [Transaction(TransactionMode.Manual)]
     public class CalcBuilderCommand : IExternalCommand
     {
+        static CalcBuilderCommand()
+        {
+            // dependencies are resolved at the point of command loading rather than command execution
+            // so we implement the assembly resolve event here
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             try
             {
                 App.RevitVersion = commandData.Application.Application.VersionNumber;
-                AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
                 UIDocument uidoc = commandData.Application.ActiveUIDocument;
                 Document doc = commandData.Application.ActiveUIDocument.Document;
 
@@ -54,18 +52,27 @@ namespace Calc.RevitApp.Revit
         }
 
 
-        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
-            // depending on the build mode, use different paths
-            string assemblyFolder = @"C:\source\calc\bin";
-            //string assemblyFolder = $"C:\\ProgramData\\Autodesk\\Revit\\Addins\\{App.RevitVersion}\\CalcRevit"; // Specify the directory where your DLLs are located
+            // for general case
+            string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            // for pyrevit invoked case
+            string assemblyLocationHdM = "C:\\HdM-DT\\RevitCSharpExtensions\\calc-test-bin\\bin";
+
+            string assemblyFolder = string.IsNullOrEmpty(assemblyLocation) ?
+                assemblyLocationHdM : Path.GetDirectoryName(assemblyLocation);
             string assemblyName = new AssemblyName(args.Name).Name;
             string assemblyPath = Path.Combine(assemblyFolder, assemblyName + ".dll");
 
             if (File.Exists(assemblyPath))
+            {
                 return Assembly.LoadFrom(assemblyPath);
 
-            return null;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
