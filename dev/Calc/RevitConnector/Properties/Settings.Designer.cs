cc751abba2554045f35a546fc12d4a639af532b9
k.luo <k.luo@herzogdemeuron.com>
2024-05-07 22:56:38 +0200

    update bin

--- dev/Calc/RevitConnector/Properties/Settings.Designer.cs
+++ dev/Calc/RevitConnector/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Calc.MVVM.Properties {
+namespace Calc.RevitConnector.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
