d63453167b07ce69a8d75d8ae021cdc6006e4cb2
k.luo <k.luo@herzogdemeuron.com>
2024-06-26 16:30:59 +0200

    adding buildup selection view

---
+++ dev/Calc/MVVM/ViewModels/BuildupSelectionViewModel.cs
@@ -0,0 +1,224 @@
+﻿using Calc.Core;
+using Calc.Core.Objects.Materials;
+using Calc.MVVM.Helpers;
+using Calc.MVVM.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Data;
+
+namespace Calc.MVVM.ViewModels
+{
+
+    public class BuildupSelectionViewModel : INotifyPropertyChanged
+    {
+        private readonly List<Material> allMaterials;
+        public ICollectionView AllMaterialsView { get; set; }
+        public List<StandardModel> AllStandards { get; set; }
+        private List<FilterTagModel> materialTags;
+        public ICollectionView MaterialTagsView
+        {
+            get
+            {
+                var view = CollectionViewSource.GetDefaultView(materialTags);
+                if(view is ListCollectionView listCollectionView)
+                {
+                    listCollectionView.CustomSort = new FilterTagComparer();
+                }
+                return view;
+            }
+        }
+        private List<FilterTagModel> productTags;
+        public ICollectionView ProductTagsView
+        {
+            get
+            {
+                var view = CollectionViewSource.GetDefaultView(productTags);
+                if (view is ListCollectionView listCollectionView)
+                {
+                    listCollectionView.CustomSort = new FilterTagComparer();
+                }
+                return view;
+            }
+        }
+        private string currentSearchText;
+
+        private Material selectedMaterial;
+        public Material SelectedMaterial
+        {
+            get => selectedMaterial;
+            set
+            {
+                if (value == selectedMaterial) return;
+                selectedMaterial = value;
+                OnPropertyChanged(nameof(SelectedMaterial));
+            }
+        }
+
+        private FilterTagModel selectedMaterialTag;
+        public FilterTagModel SelectedMaterialTag
+        {
+            get => selectedMaterialTag;
+            set
+            {
+                selectedMaterialTag = value;
+                FilterMaterialsWithType();
+                UpdateDynamicCounts();
+                OnPropertyChanged(nameof(SelectedMaterialTag));
+            }
+        }
+
+        private FilterTagModel selectedProductTag;
+        public FilterTagModel SelectedProductTag
+        {
+            get => selectedProductTag;
+            set
+            {
+                selectedProductTag = value;
+                FilterMaterialsWithType();
+                UpdateDynamicCounts();
+                OnPropertyChanged(nameof(SelectedProductTag));
+            }
+        }
+
+        public BuildupSelectionViewModel(DirectusStore store)
+        {
+            AllMaterialsView = CollectionViewSource.GetDefaultView(store.MaterialsAll);
+            allMaterials = store.MaterialsAll;
+            AllStandards = new List<StandardModel>(store.StandardsAll.Select(s => new StandardModel(s)));
+            InitTypeTags();
+
+        }
+
+        public void Reset()
+        {
+            SelectedMaterialTag = null;
+            SelectedProductTag = null;
+            SelectedMaterial = null;
+        }
+
+        public void PrepareMaterialSelection(Material material)
+        {
+            currentSearchText = "";
+            SelectedMaterialTag = null;
+            SelectedProductTag = null;
+            SelectedMaterial = material;
+        }
+
+        public void HandleSourceCheckChanged()
+        {
+            FilterMaterialsWithType();
+        }
+
+        private void InitTypeTags()
+        {
+            materialTags = new List<FilterTagModel>();
+            productTags = new List<FilterTagModel>();
+            foreach (var material in allMaterials)
+            {
+                var mType = material.MaterialTypeFamily ?? "?";
+                var pType = material.ProductTypeFamily ?? "?";
+                if (!materialTags.Exists(t => t.TagName == mType))
+                {
+                    materialTags.Add(new FilterTagModel(mType));
+                }
+                if (!productTags.Exists(t => t.TagName == pType))
+                {
+                    productTags.Add(new FilterTagModel(pType));
+                }
+
+                // add relation count to each other
+                var mTag = materialTags.Find(t => t.TagName == mType);
+                var pTag = productTags.Find(t => t.TagName == pType);
+                mTag.AddRelationCount(pTag);
+                pTag.AddRelationCount(mTag);
+
+            }
+        }
+
+        private void FilterMaterialsWithType()
+        {
+            AllMaterialsView.Filter = (obj) =>
+            {
+                var material = obj as Material;
+                // either name, material type or material type family contains the search text
+                var name = material.Name?.ToLower() ?? "";
+                var standard = material.Standard.Name ?? "";
+                var mType = material.MaterialType?.ToLower() ?? "";
+                var mFamily = material.MaterialTypeFamily?.ToLower() ?? "";
+                var pType = material.ProductTypeFamily?.ToLower() ?? "";
+                var pFamily = material.ProductTypeFamily?.ToLower() ?? "";
+
+                if (!AllStandards.Where(s => s.IsSelected).Any(s => s.Name == standard))
+                {
+                    return false;
+                }
+
+                if (SelectedMaterialTag != null && (material.MaterialTypeFamily ??"?") != SelectedMaterialTag.TagName)
+                {
+                    return false;
+                }
+                if (SelectedProductTag != null && (material.ProductTypeFamily ?? "?") != SelectedProductTag.TagName)
+                {
+                    return false;
+                }
+                if (!string.IsNullOrEmpty(currentSearchText) 
+                    && !name.Contains(currentSearchText) 
+                    && !mType.Contains(currentSearchText) 
+                    && !mFamily.Contains(currentSearchText)
+                    && !pType.Contains(currentSearchText)
+                    && !pFamily.Contains(currentSearchText))
+                {
+                    return false;
+                }
+                return true;
+            };
+        }
+
+        /// <summary>
+        /// dynamic count is the count of each tag where the other type is selected
+        /// if a tag of material type is selected, 
+        /// the dynamic count of product type is the count of all materials with the selected material type.
+        /// if a tag of the same type group is selected, all the other tags of the same type group will have null dynamic count
+        /// the dynamic count is filtered with both selected tags
+        /// </summary>
+        private void UpdateDynamicCounts()
+        {
+            UpdateOtherGroupDynamicCounts(selectedProductTag, materialTags);
+            UpdateOtherGroupDynamicCounts(selectedMaterialTag, productTags);
+            UpdateSameGroupDynamicCounts(selectedMaterialTag, materialTags);
+            UpdateSameGroupDynamicCounts(selectedProductTag, productTags);
+        }
+
+        private void UpdateOtherGroupDynamicCounts(FilterTagModel selectedTag, List<FilterTagModel> allTags)
+        {
+            foreach (var tag in allTags)
+            {
+                tag.UpdateDynamicCount(selectedTag);
+            }
+        }
+
+        private void UpdateSameGroupDynamicCounts(FilterTagModel selectedTag, List<FilterTagModel> allTags)
+        {
+            foreach(var tag in allTags)
+            {
+                if (tag != selectedTag && selectedTag!=null)
+                {
+                    tag.CleanDynamicCount();
+                }
+            }
+        }
+
+        public void HandleSearchTextChanged(string currentText)
+        {
+            currentSearchText = currentText.ToLower();
+            FilterMaterialsWithType();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ dev/Calc/MVVM/Views/BuildupSelectionView.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Calc.Core.Objects.Materials;
+using Calc.MVVM.Helpers;
+using Calc.MVVM.ViewModels;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Calc.MVVM.Views
+{
+    public partial class BuildupSelectionView : Window
+    {
+        private double originalLeft; // used to store the original left position of the window, for window ajustment when expander is expanded or collapsed
+
+        private void WindowLocationChanged(object sender, RoutedEventArgs e)
+        {
+            originalLeft = this.Left;
+        }
+
+        private void WindowLocationChanged(object sender, EventArgs e)
+        {
+            originalLeft = this.Left;
+        }
+
+        private readonly BuildupSelectionViewModel BuildupSelectionVM;
+        public Material SelectedMaterial { get => BuildupSelectionVM.SelectedMaterial; }
+
+        public BuildupSelectionView(BuildupSelectionViewModel buildupSelectionVM)
+        {
+            this.BuildupSelectionVM = buildupSelectionVM;
+            this.DataContext = BuildupSelectionVM;
+            //InitializeComponent();
+        }
+
+        private void SearchTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var currentText = (sender as TextBox).Text;
+            BuildupSelectionVM.HandleSearchTextChanged(currentText);
+        }
+
+        private void ListViewSelected(object sender, MouseButtonEventArgs e)
+        {
+            var item = (sender as ListView).ContainerFromElement(e.OriginalSource as DependencyObject) as ListViewItem;
+            if (item != null && item.IsSelected)
+            {
+                item.IsSelected = false;
+                e.Handled = true;
+            }
+        }
+
+        private void ListViewLoaded(object sender, RoutedEventArgs e)
+        {
+            var listView = sender as ListView;
+            if(listView.SelectedItem!=null)
+            {
+                listView.ScrollIntoView(listView.SelectedItem);
+            }
+        }
+
+        private void ListViewItemMaterialDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void OKClicked(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void CancelClicked(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Enables scrolling item by item in the list view
+        /// </summary>
+        private void ListViewPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer viewer = ViewHelper.FindVisualChild<ScrollViewer>(sender as ListView);
+
+            if (e.Delta > 0)
+            {
+                viewer.LineUp();
+            }
+            if (e.Delta < 0)
+            {
+                viewer.LineDown();
+            }
+            e.Handled = true;
+        }
+
+        private void SourceCheckChanged(object sender, RoutedEventArgs e)
+        {
+            BuildupSelectionVM.HandleSourceCheckChanged();
+        }
+
+        private void Expander_Expanded(object sender, RoutedEventArgs e)
+        {
+            Expander expander = sender as Expander;
+            if (expander != null)
+            {
+                this.Width += 220;
+                this.Left = originalLeft; // Keep the left position fixed
+            }
+        }
+
+        private void Expander_Collapsed(object sender, RoutedEventArgs e)
+        {
+            Expander expander = sender as Expander;
+            if (expander != null)
+            {
+                this.Width -= 220;
+                this.Left = originalLeft; // Keep the left position fixed
+            }
+        }
+    }
+}
