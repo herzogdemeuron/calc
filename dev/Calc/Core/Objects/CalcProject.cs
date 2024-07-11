262a0e14003a97dc9c046e9e73d3d68df389519a
kekejun <k.luo@herzogdemeuron.com>
2024-07-11 16:20:59 +0200

    clean

--- dev/Calc/Core/Objects/CalcProject.cs
+++ dev/Calc/Core/Objects/CalcProject.cs
@@ -1,8 +1,5 @@
-﻿using Calc.Core.Objects.Standards;
-using Newtonsoft.Json;
-using System;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Calc.Core.Objects
 {
