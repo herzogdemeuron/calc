0b9d66cf28ff889cee79f2b0dc8e07c4e45cc800
kekejun <k.luo@herzogdemeuron.com>
2024-10-03 16:29:39 +0200

    simplified assembly class

--- dev/Core/Objects/Assemblies/Assembly.cs
+++ dev/Core/Objects/Assemblies/Assembly.cs
@@ -3,104 +3,45 @@ using Calc.Core.Objects.Standards;
 using Calc.Core.Snapshots;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace Calc.Core.Objects.Assemblies
 {
-    public class Assembly : INotifyPropertyChanged, ISearchable  // check: notify property needed?
+    public class Assembly : ISearchable
     {
-
-        private int id;
         [JsonProperty("id")]
-        public int Id
-        {
-            get => id;
-            set => SetProperty(ref id, value);
-        }
-
+        public int Id { get; set; }
         [JsonProperty("standards")]
-        public List<StandardItem> StandardItems { get; set; } // this is only for deserialization
-
+        public List<StandardItem> StandardItems { get; set; }
         [JsonIgnore]
         public string StandardsString { get => StandardItems.Select(x => x.Standard.Name).Aggregate((x, y) => x + ", " + y); }
-
-        private string code;
         [JsonProperty("code")]
-        public string Code
-        {
-            get => code;
-            set => SetProperty(ref code, value);
-        }
-
-        private string name;
+        public string Code { get; set; }
         [JsonProperty("name")]
-        public string Name
-        {
-            get => name;
-            set => SetProperty(ref name, value);
-        }
-
-        private Unit assemblyUnit;
+        public string Name { get; set; }
         [JsonProperty("assembly_unit")]
-        public Unit AssemblyUnit
-        {
-            get => assemblyUnit;
-            set => SetProperty(ref assemblyUnit, value);
-        }
-
-        private AssemblyGroup group;
+        public Unit AssemblyUnit { get; set; }
         [JsonProperty("group")]
-        public AssemblyGroup Group
-        {
-            get => group;
-            set => SetProperty(ref group, value);
-        }
-
+        public AssemblyGroup Group { get; set; }
         public string GroupName => Group?.Name;
-
         [JsonProperty("carbon_a1a3")]
         public double? AssemblyGwp { get; set; }
-
         [JsonProperty("grey_energy_fabrication_total")]
         public double? AssemblyGe { get; set; }
-
         [JsonProperty("description")]
         public string Description { get; set; }
         [JsonProperty("verified")]
         public bool Verified { get; set; }
-
         [JsonProperty("image")]
         public AssemblyImage AssemblyImage { get; set; }
         [JsonProperty("speckle_project_id")]
         public string SpeckleProjectId { get; set; }
         [JsonProperty("speckle_model_id")]
         public string SpeckleModelId { get; set; }
-
+        [JsonProperty("calculation_components")]
+        public List<CalculationComponent> CalculationComponents { get; set; }
         [JsonIgnore]
         public AssemblySnapshot AssemblySnapshot { get; set; }
-
-
-        private List<CalculationComponent> calculationComponents = new List<CalculationComponent>();
-        [JsonProperty("calculation_components")]
-        public List<CalculationComponent> CalculationComponents
-        {
-            get => calculationComponents;
-            set => SetProperty(ref calculationComponents, value);
-        }
-
-        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null) // is this needed?
-        {
-            if (EqualityComparer<T>.Default.Equals(storage, value))
-            {
-                return false;
-            }
-
-            storage = value;
-            OnPropertyChanged(propertyName);
-            return true;
-        }
 
         public override string ToString()
         {
@@ -119,13 +60,6 @@ namespace Calc.Core.Objects.Assemblies
         public override int GetHashCode()
         {
             return Id.GetHashCode();
-        }
-
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
